a4ba6cc663007f87f43f72477e2626cbaf9ad10c
RidvanOzturk <ozturkridvan1@gmail.com>
2024-05-14 18:19:16 +0300

    added AddFeeks İmage

--- lib/Screens/AddFeelScreen.dart
+++ lib/Screens/AddFeelScreen.dart
@@ -1,9 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
-class AddFeelScreen extends StatelessWidget {
+class AddFeelScreen extends StatefulWidget {
+  @override
+  _AddFeelScreenState createState() => _AddFeelScreenState();
+}
+
+class _AddFeelScreenState extends State<AddFeelScreen> {
   final TextEditingController _feelController = TextEditingController();
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  void _shareFeel(BuildContext context) async {
+    String feel = _feelController.text.trim();
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+    if (feel.isNotEmpty && userId.isNotEmpty) {
+      CollectionReference feelsRef = FirebaseFirestore.instance.collection('feels');
+
+      Map<String, dynamic> feelData = {
+        'userId': userId,
+        'feel': feel,
+        'timestamp': FieldValue.serverTimestamp(),
+      };
+
+      if (_image != null) {
+        // Eğer bir resim seçilmişse, resmi Firebase Storage'a yükleyin
+        String fileName = 'images/${DateTime.now().millisecondsSinceEpoch}.jpg';
+        UploadTask uploadTask = FirebaseStorage.instance
+            .ref(fileName)
+            .putFile(_image!);
+
+        TaskSnapshot snapshot = await uploadTask;
+        String downloadUrl = await snapshot.ref.getDownloadURL();
+        feelData['imageUrl'] = downloadUrl;
+      }
+
+      feelsRef.add(feelData).then((_) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Hissiniz başarıyla paylaşıldı.'),
+          backgroundColor: Colors.green,
+        ));
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Hissinizi paylaşırken bir hata oluştu: $error'),
+          backgroundColor: Colors.red,
+        ));
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Lütfen bir his yazın.'),
+        backgroundColor: Colors.red,
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +95,18 @@ class AddFeelScreen extends StatelessWidget {
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Düşüncelerinizi buraya yazın',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.photo),
+                  onPressed: _pickImage,
+                ),
               ),
             ),
+            SizedBox(height: 10),
+            if (_image != null)
+              Image.file(
+                _image!,
+                height: 150,
+              ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
@@ -45,40 +118,5 @@ class AddFeelScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _shareFeel(BuildContext context) {
-    String feel = _feelController.text.trim();
-    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-
-    if (feel.isNotEmpty && userId.isNotEmpty) {
-
-      CollectionReference feelsRef = FirebaseFirestore.instance.collection('feels');
-
-
-      feelsRef.add({
-        'userId': userId,
-        'feel': feel,
-        'timestamp': FieldValue.serverTimestamp(),
-      }).then((_) {
-
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Hissiniz başarıyla paylaşıldı.'),
-          backgroundColor: Colors.green,
-        ));
-      }).catchError((error) {
-
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text('Hissinizi paylaşırken bir hata oluştu: $error'),
-          backgroundColor: Colors.red,
-        ));
-      });
-    } else {
-
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Lütfen bir his yazın.'),
-        backgroundColor: Colors.red,
-      ));
-    }
   }
 }
