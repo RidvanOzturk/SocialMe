792da79578f9eb466276cde2d6c1f2e1b52f613d
RidvanOzturk <ozturkridvan1@gmail.com>
2024-06-10 15:16:36 +0300

    edited main.dart and index.html for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_me/Screens/LoginScreen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   @override
